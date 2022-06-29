d62784b21e9c2539503512f0410030a8467d356a
dschro-1993 <dominik.schroettinger@devoteam.com>
2022-06-29 19:56:58 +0200

    minor fixes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as aws from '@pulumi/aws';
 
-// org, project, env can be passed as config (based on stack)
+// org, project, env should be passed as config (based on stack)
 const stackReference = new pulumi.StackReference('org-project-provider-env');
 
 stackReference.getOutput('vpcId').apply((vpcId) => {
